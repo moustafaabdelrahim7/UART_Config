c24f6fb705f4cdc13f86bb954e1e38ed7922947b
moustafaabdelrahim7 <128411699+moustafaabdelrahim7@users.noreply.github.com>
2023-05-10 23:14:34 +0300

    Add files via upload

---
+++ UART/uart_config.h
@@ -0,0 +1,35 @@
+
+
+
+#ifndef UART_CONFIG_H_
+#define UART_CONFIG_H_
+
+
+
+#define Speed_Modes_Selection			  normal_speed
+
+	
+#define Parity_Selection				  NO_PARITY   
+
+
+#define Type_Of_Synchronism				  ASYNCH 
+
+	
+#define Stop_Bits_Selection				  ONE_STOP_BIT   
+	
+
+#define  data_Size_Selection			 _8_DATA_BITS    
+
+
+#define CPU_frequency_Selection			 _8_MHZ 
+
+
+#define baud_rate_Selection				 51 
+
+#define Enable_Selection				Enable_TX_RX  
+
+
+#endif 
+ 
+
+
